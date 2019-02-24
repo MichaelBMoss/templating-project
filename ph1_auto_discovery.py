1560c2e5abfdedc54cb011dea3262995d7228f9e
Mike Moss' Ubuntu Linux <michaelbmoss@gmail.com>
2019-02-24 13:44:10 -0800

    still on phase 1

--- ph1_auto_discovery.py
+++ ph1_auto_discovery.py
@@ -1,28 +1,31 @@
-def main():
-    for page in pages:
+def create_list():
+    import glob
+    all_html_files = glob.glob("content/*.html")
+    pages = []
+    import os
+    for entry in all_html_files:
+        file_path = entry
+        file_name = os.path.basename(file_path)
+        name_only, extension = os.path.splitext(file_name)
+        pages.append({
+        "filename": "content/" + name_only + extension,
+        "title": name_only,
+        "output": "docs/" + name_only + extension,
+        "link": name_only + '_ph',
+    })
+    return pages
+
+def create_pages():
+    for page in create_list():
         template = open("templates/base.html").read()
         content = open(page['filename']).read()
         combined1 = template.replace("{{content_placeholder}}", content)
         combined2 = combined1.replace("{{title_ph}}", page['title'])
         combined3 = combined2.replace(page['link'], "box")
         open(page['output'], "w+").write(combined3)
-
-
-import glob
-all_html_files = glob.glob("content/*.html")
-
-pages = []
-import os
-for entry in all_html_files:
-    file_path = entry
-    file_name = os.path.basename(file_path)
-    name_only, extension = os.path.splitext(file_name)
-    pages.append({
-    "filename": "content/" + name_only + extension,
-    "title": name_only,
-    "output": "docs/" + name_only + extension,
-    "link": name_only + '_ph',
-})
-
+        
+def main():
+    create_pages()
 
 main()
+       
